9d5f42b3ea0ded871b2d32c5363edda9de28201d
nititas <laosawat_n@silpakorn.edu>
2017-02-13 22:56:13 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style-home.css">
+	
+
+</head>
+<body>	
+		<div class="box-bg">
+			<div class="menu-top"></div>
+			<div class="menu-list"></div>
+			<div class="menu"></div>
+			
+			
+
+		</div>
+
+
+</body>
+</html>
